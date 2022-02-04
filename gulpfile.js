29b893440770159425ca95a05385c279842c950a
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2022-02-04 14:56:05 +0100

    Re escritura del proyecto
    Inicio del proyecto ajustado a nuevas tecnologías y módulos php

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,100 @@
-const { series, src, dest, watch }= require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+// CSS
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Imagenes
+const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
-const cssnano = require('gulp-cssnano');
+const avif = require('gulp-avif');
+const svg = require('gulp-svgmin');
 
+// Javascript
+const terser = require('gulp-terser-js');
 
-
-function imagenes(){
-    return src('./img/prev/**/*')
-        .pipe( imagemin())
-        .pipe( dest('./img/after'))
+const path = {
+    scss: 'src/scss/**/*.scss',
+    css: 'build/css/app.css',
+    js: 'src/js/**/*.js',
+    img: 'src/img/**/*.{jpg,png}',
+    imgmin: 'build/img/**/*.{jpg,png}',
+    svg: 'src/img/**/*.svg'
 }
 
-function versionWebp(){
-    return src('./img/after/**/*')
-        .pipe(webp())
-        .pipe(dest('./img/after/webp'))
+function css( done ) {
+    src(path.scss) // Identificar el archivo .SCSS a compilar
+        .pipe(sourcemaps.init())
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
+        .pipe( dest('build/css') ) // Almacenarla en el disco duro
+    done();
 }
 
-function css (){
-    return src('./css/*.css')
-        .pipe(cssnano())
-        .pipe( dest('./css/nano') )
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    done();
 }
 
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionAvif( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function javascript( done ) {
+    src(path.js)
+        .pipe(sourcemaps.init())
+        .pipe( terser() )
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+
+    done();
+}
+
+function imgSvg(){
+    return src(path.svg)
+        .pipe(svg())
+        .pipe(dest('build/img'));
+}
+
+function dev( done ) {
+    watch(path.scss, css);
+    watch(path.js, javascript);
+
+    done();
+}
+
+exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.css = css;
+exports.versionAvif = versionAvif;
+exports.imgSvg = imgSvg;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, imgSvg, dev);
